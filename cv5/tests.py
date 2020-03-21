da717a7fe0e69e4eea344c579d6b4b4faa8ba705
Savlar <matusgal@outlook.com>
2020-03-21 16:21:13 +0100

    added new test

--- cv5/tests.py
+++ cv5/tests.py
@@ -14,13 +14,19 @@ class DataTests(unittest.TestCase):
     def test_question_number(self):
         i = 1
         for row in self.parser.answers:
-            self.assertEqual(self.parser.question_order(row), i)
+            self.assertEqual(self.parser.question_order(row), i, 'Missing answer number: ' + str(i))
             i += 1
 
     def test_answer_is_digit(self):
         for row in self.parser.answers:
             for number in row.split(';'):
-                self.assertTrue(self.parser.is_number(number), 'Wrong number: ' + repr(number))
+                self.assertTrue(self.parser.is_number(number), 'Answer is not number: ' + repr(number))
+
+    def test_number_is_correct_choice(self):
+        for row in self.parser.answers:
+            for number in row.split(';'):
+                if self.parser.is_number(number):
+                    self.assertIn(int(number), [0, 1, 2, 3, 4, 5, 6, 7, 99], 'Number ' + number + ' is not correct answer')
 
 
 if __name__ == '__main__':
@@ -30,4 +36,5 @@ if __name__ == '__main__':
 # chyby
 # - chybajuce cislo v dotazniku
 # - mensi pocet odpovedi ako ma byt
-# - nespravne cislo v odpovedi na otazku
+# - nespravne odpoved na otazku (prazdne, string, desatinne, ...)
+# - nespravne cislo odpovede
